26f336a6ae8b7f0870ed03f6cf631cb6b72f140f
“SantiagoPerez“ <saantiiago50@gmail.com>
2025-06-10 14:37:55 -0300

    Proyecto JavaScript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const academia = "CoderHouse"
+
+let curso = prompt ("Ingrese la Materia")
+let comision = prompt ("Ingrese el numero de comision")
+
+console.log("Materia: "+curso+" Comision N*: "+comision+ " Academia "+academia)
+
+let numeroA = parseInt(prompt("Ingrese el primer numero"))
+let numeroB =parseInt(prompt("Ingrese el segundo numero"))
+
+let resultado = numeroA * numeroB
+console.log(resultado)
